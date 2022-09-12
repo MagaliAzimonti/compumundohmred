cbe70e6f53de16a01054b583349a988b4209eded
Magalí Azimonti <develop.magaliazimonti@gmail.com>
2022-09-12 14:19:58 -0300

    resetpruebavercel

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -15,8 +15,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig, {
-  experimentalForceLongPolling: true,
-  useFetchStreams: false,
-});
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
